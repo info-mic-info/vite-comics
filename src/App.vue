2621a761fa7c41a36f75537ee774dec61b9a55f0
info-mic-info <jobsocial94@gmail.com>
2023-01-11 16:22:18 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+    <script>
+import AppHeader from './components/AppHeader.vue'
+    export default {
+      components:{
+        AppHeader
+      }
+    }
+    </script>
+
+<template lang="">
+
+ <AppHeader />
+ 
+</template>
+
+<style lang="">
+  
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  
+}
+</script>
+
+<template lang="">
+
+<header>
+    Ciao
+</header>
+
+</template>
+
+<style lang="">
+
+</style>
